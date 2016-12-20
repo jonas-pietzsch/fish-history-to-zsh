47f73c8d6f5611e11a2f2f937dd57e5ff83ecff3
Verhoelen Jonas <jonas.verhoelen@codecentric.de>
2016-12-20 12:49:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let getUserHome = () => {
+  return process.env.HOME || process.env.USERPROFILE;
+}
+
+/*
+  Fish history (~/.local/share/fish/fish_history) is in YAML format.
+  Parsed to JS object it looks like:
+
+  [{
+    cmd: './some-command',
+    when: 123123123
+  }, ...]
+
+
+  Zsh history looks like:
+  : 1482183081:0;git clone https://github.com/powerline/fonts.git
+ */
+
+let zshHistory = require('yamljs')
+  .load(getUserHome() + '/.local/share/fish/fish_history')
+  .map(entry => `: ${entry.when}:0;${entry.cmd}`)
+  .join('\n');
+
+
+// log to stdout to append the converted fish history to ~/.zsh_history
+console.log(zshHistory);
